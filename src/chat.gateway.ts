4a8b1468045cd90578d73c95a64d62cb1b95a681
alireza kargar <imanesche@gmail.com>
2022-08-25 01:41:32 +0430

    fixed cors error in nest js

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,12 +1,27 @@
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 
-@WebSocketGateway()
+@WebSocketGateway({
+    allowEIO3: true,
+    cors: {
+        origin: true,
+        credentials: true
+    },
+})
 export class chatGateway {
+    public test
+    constructor() {
+        this.test = false
+    }
     @WebSocketServer()
     server
 
     @SubscribeMessage('message')
     handleMessage(@MessageBody() message: string): void {
-        this.server.emit("message", message)
+        if (this.test) {
+            console.log("false")
+            return
+        }
+        this.test = true
+        // this.server.emit("message", message)
     }
 }
